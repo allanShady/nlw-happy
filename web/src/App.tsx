ff76a74e73176c4a9a6c3612cb28b009fcc3636d
Alan Camilo <acamilo.ag@gmail.com>
2020-10-13 11:25:29 +0200

    fix: fix build bug, Title is defined and not used

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,6 @@
 import React from 'react';
 import Routes from './routes'
 
-
-interface TitleProps {
-  text: string
-}
-
-function Title(props: TitleProps) {
-  return (
-  <h1>{props.text}</h1>
-  )
-}
-
 function App() {
   return (
     <Routes />
