255f04abb4bb267a6ba3446877169ec94102743f
Alan Camilo <acamilo.ag@gmail.com>
2020-10-14 19:50:24 +0200

    refactor: abstract app routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,39 +1,12 @@
 import express, { json } from 'express'
-import { getRepository } from 'typeorm'
-
 import './database/connection';
-import Orphanage from './models/Orphanage';
+import './routes'
+import routes from './routes';
+
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-    
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    })
-
-    await orphanagesRepository.save(orphanage);
-
-    return response.status(201).json(orphanage);
-})
+app.use(routes);
 
 app.listen(3333);
