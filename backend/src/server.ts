4b2dfceaea02be3fe41dfb6510522e724f861539
Alan Camilo <acamilo.ag@gmail.com>
2020-10-16 16:24:47 +0200

    fix: properly enable CORS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,11 +9,9 @@ import routes from './routes';
 import errorHandler from './errors/handler'
 
 const app = express();
-//app.use(cors);
-
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(errorHandler);
-
 app.listen(3333);
