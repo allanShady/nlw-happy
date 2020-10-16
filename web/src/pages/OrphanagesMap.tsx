3e392f350e490ea49e72b6a26627df4382df50d5
Alan Camilo <acamilo.ag@gmail.com>
2020-10-16 10:50:53 +0200

    refactor: abstract sidebar

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map } from 'react-leaflet'
-import { TileLayer } from 'react-leaflet'
-
-import '../styles/global.css'
-import '../styles/pages/orphanages-map.css'
-import 'leaflet/dist/leaflet.css'
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
 
 import mapMarkerImg from '../images/map-marker.svg';
- 
+import '../styles/pages/orphanages-map.css'
+import mapIcon from '../utils/mapIcon';
+
+
 function OrphanageMap() {
     return (
         <div id="page-map">
@@ -31,10 +29,30 @@ function OrphanageMap() {
                 zoom={14}
                 style={{width: '100%', height: '100%'}}>
                 {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>*/}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                <TileLayer 
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker
+                    icon={mapIcon} 
+                    position={[-25.7406925,32.5766883]} >
+
+                    <Popup closeButton={false}
+                        maxWidth={240}
+                        minWidth={240}
+                        className="map-popup"    
+                    >
+                        Lar dos deficientes
+                        <Link to="/orphanages/1"> 
+                            <FiArrowRight  size={20} color="#FFF" /> 
+                            </Link>
+                        
+                    </Popup> 
+                </Marker>
+                    
+            
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size="32" color="#FFF" /> 
             </Link>
         </div>
