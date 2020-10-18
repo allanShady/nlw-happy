3f4bfe8137e1041c218fd67cf1a6a5bf23d781eb
Alan Camilo <acamilo.ag@gmail.com>
2020-10-18 16:52:01 +0200

    feat: add routes to details and create orphanage

--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -6,13 +6,30 @@ const { Navigator, Screen }  = createStackNavigator();
 
 import OrphanagesMap from './pages/OrphanagesMap'
 import OrphanageDetails from './pages/OrphanageDetails'
+import OrphanageData from './pages/CreateOrphanage/OrphanageData'
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition'
+import Header from './components/Header';
+
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{headerShown: false, cardStyle: {backgroundColor: '#f2f3f5' }}}>
                 <Screen name="OrphanagesMap" component={OrphanagesMap} />
-                <Screen name="OrphanageDetails" component={OrphanageDetails} />
+                <Screen 
+                    name="OrphanageDetails" 
+                    component={OrphanageDetails}
+                    options={{ headerShown: true , header: ()=> <Header title="Orfanato"/> }} />
+
+                <Screen 
+                    name="SelectMapPosition" 
+                    component={SelectMapPosition} 
+                    options={{headerShown: true, header: () => <Header showCancel={false} title="Selecione o mapa" />}} />
+                <Screen 
+                    name="OrphanageData" 
+                    component={OrphanageData} 
+                    options={{ headerShown: true , header: ()=> <Header title="Entre com os dados"/> }} />
+                
             </Navigator>
         </NavigationContainer>
     );
